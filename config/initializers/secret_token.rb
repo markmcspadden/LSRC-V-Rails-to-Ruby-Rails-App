2e9496b9fa40086a2a1d7268f65529b93c8e611a
Mark McSpadden <markmcspadden@gmail.com>
2011-08-02 23:20:35 -0500

    Add Rails 3.0.4 skeleton app and fill out a bit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dashboard::Application.config.secret_token = '20f3708f8700b848f93c9966e39554822c6c89c2878e1348ede0540d8184832099765206740590fe89dd8c86968ef931cfc6dd3444be4f458005e4d4e08a6830'
