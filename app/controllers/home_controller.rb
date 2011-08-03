503425f3322d2de88ea9009e78b4047a38b714e1
Mark McSpadden <markmcspadden@gmail.com>
2011-08-02 23:49:06 -0500

    Add schedule layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    
+    @schedule_items = ScheduleItem.all
   end
   
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  
+  def display_time(time)
+    time.strftime("%H:%M")
+  end
+  
 end
