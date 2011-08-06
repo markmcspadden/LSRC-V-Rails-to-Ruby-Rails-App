8adeccd8415ee69c5544dd891793c09fd60d11f9
Scott Johnson <7.scott.j@gmail.com>
2011-08-05 23:35:44 -0500

    clean up duck punching of render and add a partial for testing!

--- lib/extend_action_view.rb
+++ lib/extend_action_view.rb
@@ -1,18 +1,22 @@
 class ActionView::OutputBuffer
   def sirify(expression)
     reg_ex = Regexp.new(expression)
-    self.gsub(reg_ex, "Sir #{expression}") 
+    self.gsub!(reg_ex, "Sir #{expression}").gsub!(/(Sir )+/, 'Sir ') 
   end
 end
 
 class ActionController::Base
   alias_method(:org_render, :render)
   
-  def render(*args)
-    if args.empty?
+  def render(options = {}, locals = {}, &block)  
+    puts self.class
+    
+    if options.empty?
       org_render :text => render_to_string.sirify("Mark McSpadden")
     else
-      org_render(*args)
+      org_render(options, locals, &block)
     end    
   end
+  
+  
 end   
