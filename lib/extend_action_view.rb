47447e1d8d57031bc6264c2860d418d4f20be7ae
Mark McSpadden <markmcspadden@gmail.com>
2011-08-09 01:06:21 -0500

    A little help on extend_action_view so tests can run successfully for other files

--- lib/extend_action_view.rb
+++ lib/extend_action_view.rb
@@ -1,9 +1,17 @@
-class ActionView::OutputBuffer
+# class ActionView::OutputBuffer
+#   def sirify(expression)
+#     reg_ex = Regexp.new(expression)
+#     self.gsub!(reg_ex, "Sir #{expression}").gsub!(/(Sir )+/, 'Sir ') 
+#   end
+# end
+
+module ActionViewExtensions
   def sirify(expression)
     reg_ex = Regexp.new(expression)
     self.gsub!(reg_ex, "Sir #{expression}").gsub!(/(Sir )+/, 'Sir ') 
   end
 end
+ActionView::OutputBuffer.__send__(:include, ActionViewExtensions) if defined?(ActionView::OutputBuffer)
 
 class ActionController::Base
   alias_method(:org_render, :render)
