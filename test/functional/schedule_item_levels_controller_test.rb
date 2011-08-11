5895313a2b3cc525b7e00bc48ae146519178a88b
Mark McSpadden <markmcspadden@gmail.com>
2011-08-11 04:01:22 -0500

    Remove unnecessary functional tests

--- test/functional/schedule_item_levels_controller_test.rb
+++ test/functional/schedule_item_levels_controller_test.rb
@@ -1,49 +1,8 @@
 require 'test_helper'
 
 class ScheduleItemLevelsControllerTest < ActionController::TestCase
-  setup do
-    @schedule_item_level = schedule_item_levels(:one)
-  end
-
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:schedule_item_levels)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create schedule_item_level" do
-    assert_difference('ScheduleItemLevel.count') do
-      post :create, :schedule_item_level => @schedule_item_level.attributes
-    end
-
-    assert_redirected_to schedule_item_level_path(assigns(:schedule_item_level))
-  end
-
-  test "should show schedule_item_level" do
-    get :show, :id => @schedule_item_level.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @schedule_item_level.to_param
-    assert_response :success
-  end
-
-  test "should update schedule_item_level" do
-    put :update, :id => @schedule_item_level.to_param, :schedule_item_level => @schedule_item_level.attributes
-    assert_redirected_to schedule_item_level_path(assigns(:schedule_item_level))
-  end
-
-  test "should destroy schedule_item_level" do
-    assert_difference('ScheduleItemLevel.count', -1) do
-      delete :destroy, :id => @schedule_item_level.to_param
-    end
-
-    assert_redirected_to schedule_item_levels_path
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
   end
 end
